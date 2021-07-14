883ef1289cd45bcc549fcc3adb5dc15156a8d187
Raphael Pothin <raphael.pothin@gmail.com>
2021-07-14 03:21:09 +0000

    Init Enable-CloudFlows powershell function

---
+++ Scripts/Enable-CloudFlows.ps1
@@ -0,0 +1,41 @@
+Function Enable-CloudFlows {
+    <#
+        .SYNOPSIS
+            To do
+
+        .DESCRIPTION
+            To do
+
+        .PARAMETER ToDo
+            To do
+
+        .INPUTS
+            None. You cannot pipe objects to Enable-CloudFlows.
+
+        .OUTPUTS
+            Object. Enable-CloudFlows returns ...
+
+        .EXAMPLE
+            To do
+
+        .LINK
+            README.md: ...
+
+        .NOTES
+            * To do
+    #>
+
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    Param (
+        # To do
+    )
+
+    Begin{}
+
+    Process{
+        # To do
+    }
+
+    End{}
+}
